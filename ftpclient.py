d9047909db2b28bf5c3c21bcd6815f74105b3ac2
Bryan <martineb1@hawkmail.newpaltz.edu>
2017-01-04 18:04:59 -0500

    Added first error case

--- ftpclient.py
+++ ftpclient.py
@@ -1,1 +1,15 @@
 # Created by Bryan R Martinez on 1/4/2017
+from ftplib import FTP
+import sys
+
+
+def main(argv):
+    if len(argv) < 1:
+        print("Invalid num. of args entered.")
+        exit(1)
+    elif len(argv) == 1:
+        
+
+
+if __name__ == "__main__":
+    main(sys.argv)
