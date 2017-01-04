3693c87dcd2232fe1f5e15cbb2a62b971dde4c47
Bryan <martineb1@hawkmail.newpaltz.edu>
2017-01-04 18:25:05 -0500

    Added base functionality

--- ftpclient.py
+++ ftpclient.py
@@ -2,13 +2,27 @@
 from ftplib import FTP
 import sys
 
+ftp = None
+
+
+def session_loop():
+    print(ftp.getwelcome())
+    print(ftp.dir())
+    ftp.quit()
+
 
 def main(argv):
-    if len(argv) < 1:
-        print("Invalid num. of args entered.")
+    if len(argv) == 1 or len(argv) == 3:
+        print("Error: Invalid number of args entered.")
         exit(1)
-    elif len(argv) == 1:
-        
+    elif len(argv) == 2:
+        try:
+            global ftp
+            ftp = FTP(argv[1])
+            session_loop()
+        except TimeoutError:
+            print("Error: Connection timed out.")
+            exit(1)
 
 
 if __name__ == "__main__":
