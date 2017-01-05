9d3aebeb140fb6d5f9c875875a1dca1801d72371
Bryan <martineb1@hawkmail.newpaltz.edu>
2017-01-05 15:07:47 -0500

    Modified client logic

--- ftpclient.py
+++ ftpclient.py
@@ -1,11 +1,27 @@
 # Created by Bryan R Martinez on 1/4/2017
-from ftplib import FTP
+import ftplib
 import sys
 
 
 def session_loop(ftp):
-    print(ftp.getwelcome())
-    print("Enter '--exit' to quit")
+    print(str(ftp.getwelcome()).split(None, 1)[1])
+    try:
+        ftp.dir()
+    except ftplib.error_perm as e:
+        print("Error: "+str(e).split(None, 1)[1])
+        exit(1)
+    while True:
+        text = input()
+        if text.lower() == "dir":
+            ftp.dir()
+        elif text.lower().startswith("cd"):
+            try:
+                ftp.cwd(text.split(" ")[1])
+                ftp.dir()
+            except ftplib.all_errors:
+                print("Unable to access selected directory.")
+        elif text == "--exit":
+            break
     ftp.quit()
 
 
@@ -15,14 +31,14 @@ def main(argv):
         exit(1)
     elif len(argv) == 2:
         try:
-            ftp = FTP(argv[1])
+            ftp = ftplib.FTP(argv[1])
             session_loop(ftp)
         except TimeoutError:
             print("Error: Connection timed out.")
             exit(1)
     elif len(argv) == 4:
         try:
-            ftp = FTP(argv[1], argv[2], argv[3])
+            ftp = ftplib.FTP(argv[1], argv[2], argv[3])
             session_loop(ftp)
         except TimeoutError:
             print("Error: Connection timed out.")
